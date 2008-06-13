71d1eaeda21d38b9151cd01fe1e2ca6f72571b46
michael.prilop <michael.prilop@86f3544e-4d4e-0410-a5ac-691e6c45d839>
2008-06-13 09:13:03 +0000

    deactivated salt&pepper

--- includes/auth.php
+++ includes/auth.php
@@ -9,11 +9,15 @@ function authNO() {
  * verify the username and password combo from the database.
  * returns true if the user and password combo were
  ****************************/
-function authenticateUser($loginName, $pass, &$setUserID, &$admin, &$userEmail) {
+function authenticateUser($loginName, $pass, &$setUserID, &$admin, &$setUserEmail) {
 	
 	if (!isset($loginName) || !isset($pass)) {
 		return false;
 	}
+  
+  $setUserID = '';
+  $setUserEmail = '';
+  $admin = '';
 	
 	$db_handle = @ new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 	
@@ -37,27 +41,32 @@ function authenticateUser($loginName, $pass, &$setUserID, &$admin, &$userEmail) 
   $sql_query = "SELECT user.userID, user.email, user.role, pwd.pwdHash, pwd.salt
                       FROM user , pwd
                       WHERE user.userID = pwd.userID AND ".$selectUser;
-	//$sql_query = "SELECT admin, id FROM user, pwd WHERE username = ? AND password = ?";
-	
+
 	if ($stmt = $db_handle->prepare($sql_query)) {
-		$stmt->bind_param('ss', $loginName);
+		$stmt->bind_param('s', $loginName);
 		$stmt->execute();
 		$stmt->bind_result($userID, $userEmail, $isAdmin, $pwdHash, $salt);
 		$stmt->fetch();
 		
-    $digest = sha1($pass.$salt); 
-    
+    //$digest = sha1($salt.$pass); 
+    $digest = $pass;
     //check for hash = digest and userID exists and ....
-    
-		if ($userID != '') {
-			$setUserID = $userID;
-			if ($isAdmin == '1') {
-				$admin = true;
-			}
-		} else {
-			return false;
+    //use crypt()
+    if ($digest == $pwdHash) {
+      if ($userID != '') {
+        $setUserID = $userID;
+        $setUserEmail = $userEmail;
+        if ($isAdmin == '1') {
+          $admin = true;
+        }
+      } else {
+        return false; //User does not exist
+      }
+    }
+    else {
+      return false; //provided password incorrect
 		}
-		
+    
 		$stmt->free_result();
 		$stmt->close();
 	} else {
