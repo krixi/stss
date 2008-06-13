3e3d130b1f3916978964402f960b3d91cfcd1107
tjoehler <tjoehler@86f3544e-4d4e-0410-a5ac-691e6c45d839>
2008-06-13 11:40:11 +0000

    Updated login error message handling, added ability to register users

--- views/example.view.php
+++ views/example.view.php
@@ -1,8 +1,15 @@
 <?php
 
-function display($result) {
-//output here page content generated out of results storend
-//in array $result
+include BASE_PATH."/views/common.php";
+
+function display($data) {
+	showHeader(...);
+	
+	//output here page content generated out of results storend
+	//in array $data
+	
+	
+	showFooter();
 }
 
 ?>
