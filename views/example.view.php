7af57b5cb04ef3ab0d724c28dddef2a723a96033
michael.prilop <michael.prilop@86f3544e-4d4e-0410-a5ac-691e6c45d839>
2008-06-15 13:58:14 +0000

    new simpler sql querys

--- views/example.view.php
+++ views/example.view.php
@@ -3,7 +3,7 @@
 include BASE_PATH."/views/common.php";
 
 function display($data) {
-	showHeader(...);
+	showHeader(INDEX);
 	
 	//output here page content generated out of results storend
 	//in array $data
