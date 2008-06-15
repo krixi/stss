b1c1935b7ccc47cbb20d9e1dde86c04cf7fcc1bf
michael.prilop <michael.prilop@86f3544e-4d4e-0410-a5ac-691e6c45d839>
2008-06-15 20:32:11 +0000

    buy_tickets works with manual defined cart

--- trunk/views/en/buy/buy_tickets.view.php
+++ trunk/views/en/buy/buy_tickets.view.php
@@ -1,12 +1,16 @@
 <?php
 function display($data) {
 	showHeader(EVENTS);
-	
+
 	//output here page content generated out of results storend
 	//in array $data
+	if (isset($data['errors'])) {
+		foreach ($data['errors'] as $error) {
+			printf("<span class=\"error\">%s</span>\n", getString($error));
+		}
+	}
 	
-	
-	showFooter();
-}
+		showFooter();
+	}
 
-?>
+	?>
