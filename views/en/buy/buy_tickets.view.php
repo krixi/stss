57c7bdd1912c9315f69f94ae8e2164fa5493de9e
michael.prilop <michael.prilop@86f3544e-4d4e-0410-a5ac-691e6c45d839>
2008-06-16 20:53:30 +0000

    Added package ezPDF added more information to buy_tickets page and a pdf version of the bill.

--- views/en/buy/buy_tickets.view.php
+++ views/en/buy/buy_tickets.view.php
@@ -20,22 +20,97 @@ function display($data) {
 	<tr>
 		<th>Event&nbsp;name</th>
 		<th>Seat&nbsp;category</th>
+		<th>Price&nbsp;(€)</th>
 		<th>Number</th>
 		<th>Status</th>
+		<th>Total</th>
 	</tr>";
 
+		$total_sum = 0;
+
 		foreach ($data['oldCart'] AS $purchase) {
+			$sum = $purchase['number']*$purchase['price'];
+			$total_sum += $sum;
 
 			echo "<tr>\n";
 			echo "	<td> {$purchase['name']} </td>\n";
 			echo "	<td> {$purchase['category']} </td>\n";
+			echo "	<td> {$purchase['price']} </td>\n";
 			echo "	<td> {$purchase['number']} </td>\n";
 			echo "	<td> {$purchase['status']} </td>\n";
+			echo "	<td> {$sum}€ </td>\n";
 			echo "</tr>\n\n";
 		}
 
 		echo "</table>\n";
+
+		echo "<br>\n<br>\n
+		Total sum of your purchase is: $total_sum<br>\n";
+		echo "Please pay in advance per bank-transfer or pay during the event<br>\n<br>\n";
+
+		//Create pdf-document
+		require "includes/EZPDF/class.ezpdf.php";
+
+		$doc =& new Cezpdf();
+		$table= array();
+
+		$total_sum = 0;
+
+		//prepare an array to display as table
+		foreach($data['oldCart'] AS $row) {
+			$sum = $row['number']*$row['price'];
+			$total_sum += $sum;
+			$table[] = array("Name " => $row['name'],
+						"Seat category " => $row['category'],
+						"Price (€) " => $row['price'],
+						"Number " => $row['number'],
+						"Status " => $row['status'],
+						"total row (€) " => $sum);
+		}
+
+
+		$doc->selectFont('includes/EZPDF/fonts/Helvetica.afm');
+
+		$doc->ezImage('views/images/heading.jpg');
+		$doc->ezText("Purchased Tickets:\n", 20);
+
+		$doc->ezTable($table);
+
+		$doc->ezText("\nTotal SUM: $total_sum €\n", 15);
+		$doc->ezText("Please pay per Banktransfer:\nTo: STSS\nBankaccount #: 5334323\nBLZ: 80010030\nDeusche Bank", 10);
+
+
+		//$doc->addText('Hello World!', 50);
+
+		$pdfcode = $doc->output();
+
+		$dir = './pdf_files';
+
+		//save the file
+		if (!file_exists($dir)){
+			mkdir ($dir,0777);
+		}
+
+		$fname = $dir.'/'.date("Ymd-His").'_USER_'.$_SESSION['userID'].'.pdf';
+
+		$fp = fopen($fname,'w');
+		fwrite($fp,$pdfcode);
+		fclose($fp);
+
+
+		echo 'Bill as <a href="'.$fname.'" target="_blank">PDF</a>';
+		//	echo '
+		//<SCRIPT LANGUAGE="JavaScript"><!--
+		//function go_now ()   { window.location.href = "'.$fname.'"; }
+		////--></SCRIPT>
+		//
+		//<!--<body onLoad="go_now()"; >-->
+		//<a href="'.$fname.'" target="_blank">click here</a> if you are not re-directed.
+		//
+		//';
+
 	}
+
 
 	showFooter();
 }
