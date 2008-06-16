2b76a52b87e51d800464912738b124ccc9969119
michael.prilop <michael.prilop@86f3544e-4d4e-0410-a5ac-691e6c45d839>
2008-06-16 21:25:57 +0000

    google analytics

--- views/common.php
+++ views/common.php
@@ -88,6 +88,19 @@ include $_SESSION['lang']."/footer.html";
 printf("\n");
 ?>
 </div> <!-- end footer -->
+
+<!-- Google Analytics Code -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-4724334-1");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+<!-- END Google Analytics Code -->
+
 </body>
 </html>
 <?php
