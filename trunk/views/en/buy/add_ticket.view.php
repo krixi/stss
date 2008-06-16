3e7ec9560c3a802869bd867ca57b3d8471b769df
michael.prilop <michael.prilop@86f3544e-4d4e-0410-a5ac-691e6c45d839>
2008-06-16 21:07:48 +0000

    small minor changes (TODO's) nothing functional changed

--- trunk/views/en/buy/add_ticket.view.php
+++ trunk/views/en/buy/add_ticket.view.php
@@ -4,9 +4,10 @@ function display($data) {
 	
 	//output here page content generated out of results storend
 	//in array $data
-	printf("Added %s tickets to shopping cart.<br />\n", $data['numAdded']);
+	printf("Added %s tickets to shopping cart.<br />\n<br />\n", $data['numAdded']);
 	
-	//TODO: continue shopping or checkout tickets
+	echo "To secure your seats don't forget to check out your shopping cart 
+	<a href=\"index.php?module=buy&action=view_cart\">checkout<a><br>\n";
 	
 	showFooter();
 }
