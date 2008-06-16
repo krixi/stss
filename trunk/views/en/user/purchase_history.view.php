125a28e313d602d83b6d860210ee7511bf734f13
michael.prilop <michael.prilop@86f3544e-4d4e-0410-a5ac-691e6c45d839>
2008-06-16 10:43:10 +0000

    purchase_history

---
+++ trunk/views/en/user/purchase_history.view.php
@@ -0,0 +1,50 @@
+<?php
+
+function display($data) {
+	showHeader(PURCHASES);
+
+	//output here page content generated out of results storend
+	//in array $data
+
+
+
+
+	if(!isset($data['purchase_history'])){
+		echo "<h2>You have not yet made any purchases</h2>\n";
+	}
+	else
+	{
+		echo "<h2>Previously purchased tickets</h2>\n";
+		echo "<table>
+
+	<tr>
+		<th>Event&nbsp;name</th>
+		<th>Seat&nbsp;category</th>
+		<th>Seat&nbsp;number</th>
+		<th>Event&nbsp;date</th>
+		<th>Purchase&nbsp;date</th>	
+	</tr>";
+
+		//TODO: every second event in different color (not dependent of eventID)
+		foreach ($data['purchase_history'] AS $row) {
+			if ($row['eventID']%2) {
+				echo "<tr bgcolor=\"lightgray\">\n";
+			}
+			else {
+				echo "<tr>\n";
+			}
+			echo "	<td> {$row['name']} </td>\n";
+			echo "	<td> {$row['category']} </td>\n";
+			echo "	<td> {$row['seatID']} </td>\n";
+			echo "	<td> {$row['date']} </td>\n";
+			echo "	<td> {$row['purchaseDate']} </td>\n";
+			echo "</tr>\n\n";
+		}
+
+		echo "</table>";
+	} //end else !isset(purchase_history)
+
+	showFooter();
+}
+
+?>
