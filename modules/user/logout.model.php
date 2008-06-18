526212990728d783d5fda976022bb21281bd304c
tjoehler <tjoehler@86f3544e-4d4e-0410-a5ac-691e6c45d839>
2008-06-18 06:59:41 +0000

    Created .pdf of website flowchart, fixed "add user" english view/model to use drop down boxes instead of direct user input

--- modules/user/logout.model.php
+++ modules/user/logout.model.php
@@ -6,17 +6,20 @@ function authenticate () {
 function work() {
 	$result = array();
 	
+	$result['lang'] = $_SESSION['lang'];
 	$result['oldID'] = session_id();
 	
 	if (isset($_SESSION['user'])) {
 		$result['user'] = $_SESSION['user'];
 	}
 	
+	
 	session_destroy();
 	
 	session_start();
 	session_regenerate_id();
 	
+	$_SESSION['lang'] = $result['lang'];
 	$result['newID'] = session_id();
 	
 	return $result;
--- views/en/event/add.view.php
+++ views/en/event/add.view.php
@@ -13,41 +13,43 @@ function display($data) {
 		}
 ?>
 <h1>Add new event</h1>
-<form id="input_form" action="index.php?module=event&action=add" method="post" >
-<table>
+<form action="index.php?module=event&action=add" method="post" >
+<table class="add_event">
 <tr>
-<td>
+<td colspan="2">
 <span class="display_txt">Event name</span>
 </td>
-<td>
-<input class="textinput" name="name" id="name" type="text" size="30" />
+<td colspan="2">
+<input class="textinput" name="name" id="name" type="text" size="50" />
 </td>
+</tr>
+<tr>
 <td>
 <span class="display_txt">Occurrs:</span>
 </td>
-<td>
+<td colspan="3">
 <!-- 
 <input class="textinput" name="date" id="date" type="text" size="30" 
 value="0000-00-00 00:00" onfocus="if (this.value == '0000-00-00 00:00') this.value = '';" onblur="if (this.value == '') this.value = '0000-00-00 00:00';"/>
 -->
 <select name="month">
-<option>Jan</option>
-<option>Feb</option>
-<option>Mar</option>
-<option>Apr</option>
-<option>May</option>
-<option>Jun</option>
-<option>Jul</option>
-<option>Aug</option>
-<option>Sep</option>
-<option>Oct</option>
-<option>Nov</option>
-<option>Dec</option>
+<option value="01">Jan</option>
+<option value="02">Feb</option>
+<option value="03">Mar</option>
+<option value="04">Apr</option>
+<option value="05">May</option>
+<option value="06">Jun</option>
+<option value="07">Jul</option>
+<option value="08">Aug</option>
+<option value="09">Sep</option>
+<option value="10">Oct</option>
+<option value="11">Nov</option>
+<option value="12">Dec</option>
 </select>
 <select name="date">
 <?php
 for($i=1; $i<=31; $i++) {
-	printf("<option>%s</option>\n", $i);	
+	printf("<option>%02d</option>\n", $i);	
 }
 ?>
 </select>
@@ -58,14 +60,66 @@ for($i=2008; $i<=2050; $i++) {
 }
 ?>
 </select>
+
+time:
+<select name="time">
+<option>00:00</option>
+<option>00:30</option>
+<option>01:00</option>
+<option>01:30</option>
+<option>02:00</option>
+<option>02:30</option>
+<option>03:00</option>
+<option>03:30</option>
+<option>04:00</option>
+<option>04:30</option>
+<option>05:00</option>
+<option>05:30</option>
+<option>06:00</option>
+<option>06:30</option>
+<option>07:00</option>
+<option>07:30</option>
+<option>08:00</option>
+<option>08:30</option>
+<option>09:00</option>
+<option>09:30</option>
+<option>10:00</option>
+<option>10:30</option>
+<option>11:00</option>
+<option>11:30</option>
+<option>12:00</option>
+<option>12:30</option>
+<option>13:00</option>
+<option>13:30</option>
+<option>14:00</option>
+<option>14:30</option>
+<option>15:00</option>
+<option>15:30</option>
+<option>16:00</option>
+<option>16:30</option>
+<option>17:00</option>
+<option>17:30</option>
+<option>18:00</option>
+<option>18:30</option>
+<option>19:00</option>
+<option>19:30</option>
+<option>20:00</option>
+<option>20:30</option>
+<option>21:00</option>
+<option>21:30</option>
+<option>22:00</option>
+<option>22:30</option>
+<option>23:00</option>
+<option>23:30</option>
+</select>
 </td>
 </tr>
 <tr>
-<td colspan="2">
+<td colspan="1">
 <span class="display_txt">Description</span>
 </td>
-<td colspan="2">
-<textarea class="textinput" name="description" id="description" cols="60" rows="8"></textarea>
+<td colspan="3">
+<textarea class="textinput" name="description" id="description" cols="50" rows="8"></textarea>
 </td>
 </tr>
 <tr>
@@ -73,13 +127,13 @@ for($i=2008; $i<=2050; $i++) {
 <span class="display_txt"># Normal seats</span>
 </td>
 <td>
-<input class="textinput" name="normal" id="normal" type="text" size="30" value="<?php echo DEFAULT_NORMAL;?>" />
+<input class="textinput" name="normal" id="normal" type="text" size="25" value="<?php echo DEFAULT_NORMAL;?>" />
 </td>
 <td>
 <span class="display_txt">Price &euro;</span>
 </td>
 <td>
-<input class="textinput" name="normal_price" id="normal_price" type="text" size="30" value="<?php echo DEFAULT_NORMAL_PRICE;?>"/>
+<input class="textinput" name="normal_price" id="normal_price" type="text" size="25" value="<?php echo DEFAULT_NORMAL_PRICE;?>"/>
 </td>
 </tr>
 <tr>
@@ -87,13 +141,13 @@ for($i=2008; $i<=2050; $i++) {
 <span class="display_txt"># Premium seats</span>
 </td>
 <td>
-<input class="textinput" name="premium" id="premium" type="text" size="30" value="<?php echo DEFAULT_PREMIUM;?>"/>
+<input class="textinput" name="premium" id="premium" type="text" size="25" value="<?php echo DEFAULT_PREMIUM;?>"/>
 </td>
 <td>
 <span class="display_txt">Price &euro;</span>
 </td>
 <td>
-<input class="textinput" name="premium_price" id="premium_price" type="text" size="30" value="<?php echo DEFAULT_PREMIUM_PRICE;?>"/>
+<input class="textinput" name="premium_price" id="premium_price" type="text" size="25" value="<?php echo DEFAULT_PREMIUM_PRICE;?>"/>
 </td>
 </tr>
 </table>
