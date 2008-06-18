7ef34b122a46e3a7def6d7a508e4c381a96eb4b8
michael.prilop <michael.prilop@86f3544e-4d4e-0410-a5ac-691e6c45d839>
2008-06-18 12:22:00 +0000

    nicer welcome view

--- trunk/views/de/welcome/welcome.view.php
+++ trunk/views/de/welcome/welcome.view.php
@@ -3,10 +3,25 @@
 function display($data) {
 	showHeader(INDEX);
 	if ($data['login'] == true) {
-
-		printf("Willkommen %s,<br>\n Benutzernummer: %s<br />\n", $data['user'], $data['userID']);
+		printf("<table class=\"db_display\" id=\"cart\">\n");
+		printf("<tr>\n");
+		printf("<td><h2>Wilkommen %s</h2></td>\n", $data['user']);
+		printf("</tr>\n");
+		printf("<tr>\n");
+		printf("<td>Mehr Informationen zu unseren Veranstaltungen finden Sie unter Veranstaltungen. Dort können Sie ebenfalls Tickets kaufen. Ihre Benutzernummer ist: %s</td>\n", $data['userID']);
+		printf("</tr>\n");
+		printf("</table>\n");
+		
+		//printf("Welcome %s,<br>\nuser id: %s<br />\n", $data['user'], $data['userID']);
 	} else {
-		printf("Bitte einloggen.<br />\n");
+		printf("<table class=\"db_display\" id=\"cart\">\n");
+		printf("<tr>\n");
+		printf("<td><h2>Willkommen</h2></td>\n");
+		printf("</tr>\n");
+		printf("<tr>\n");
+		printf("<td>Bitte einloggen oder registrieren Sie sich um tickets kaufen zu können.</td>\n");
+		printf("</tr>\n");
+		printf("</table>\n");
 	}
 	showFooter();
 }
--- trunk/views/en/welcome/welcome.view.php
+++ trunk/views/en/welcome/welcome.view.php
@@ -3,10 +3,25 @@
 function display($data) {
 	showHeader(INDEX);
 	if ($data['login'] == true) {
-
-		printf("Welcome %s,<br>\nuser id: %s<br />\n", $data['user'], $data['userID']);
+		printf("<table class=\"db_display\" id=\"cart\">\n");
+		printf("<tr>\n");
+		printf("<td><h2>Welcome %s</h2></td>\n", $data['user']);
+		printf("</tr>\n");
+		printf("<tr>\n");
+		printf("<td>Please continue to browse our events and buy tickets. Your user ID is: %s</td>\n", $data['userID']);
+		printf("</tr>\n");
+		printf("</table>\n");
+		
+		//printf("Welcome %s,<br>\nuser id: %s<br />\n", $data['user'], $data['userID']);
 	} else {
-		printf("Please log in.<br />\n");
+		printf("<table class=\"db_display\" id=\"cart\">\n");
+		printf("<tr>\n");
+		printf("<td><h2>Welcome</h2></td>\n");
+		printf("</tr>\n");
+		printf("<tr>\n");
+		printf("<td>Please login or signup to buy tickets to our Events</td>\n");
+		printf("</tr>\n");
+		printf("</table>\n");
 	}
 	showFooter();
 }
