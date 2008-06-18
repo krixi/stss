21ab838730db5d9f11e0f06f9115833045d3e1bf
michael.prilop <michael.prilop@86f3544e-4d4e-0410-a5ac-691e6c45d839>
2008-06-18 10:22:29 +0000

    fixed TODO

--- trunk/views/de/user/logout.view.php
+++ trunk/views/de/user/logout.view.php
@@ -1,26 +1,6 @@
 <?php
 
 function display($data) {
-/*
-	//TODO: What about this stuff here - is it necessary?
-	// Need to restart the session because it was just destroyed
-	initSession();
-	beginHeader();
-	printf("<meta http-equiv=\"REFRESH\" content=\"2;URL=index.php\">\n");
-	endHeader(LOGOUT);
-	if (is_array($data)) {
-		if (isset($data['user'])) {
-			printf("Goodbye %s!<br />\n", $data['user']);
-		}	
-	} else {
-		printf("ERROR: Data is not an array!");
-	}
-	
-	//printf("Old Session ID:%s<br />\n", $data['oldID']);
-	//printf("New Session ID:%s<br />\n", $data['newID']);
-	
-	showFooter();
-*/
 
 header("Location: index.php");
 }
